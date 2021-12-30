690b3f29e62ef242fc78da6e8d1c5678756124d0
jhonatanascimento <jhonata_ti@outlook.com>
2021-12-30 15:02:53 -0300

    mudar imagem principal ao clicar nas miniaturas

--- js/setup.js
+++ js/setup.js
@@ -17,14 +17,22 @@ $('.owl-carousel').owlCarousel({
 
 
 const boxGame = document.getElementsByClassName("box-movie");
-const mainGame = document.getElementsByClassName("main-game");
+const mainGame = document.querySelector(".main-game");
+const gameName = document.querySelector(".game-name")
+const gameDescription = document.querySelector(".game-description")
+
 var clickedImage = '';
 
 for(var elemento of boxGame){
     elemento.addEventListener('click', function(){
         console.log(this.currentSrc)
-        clickedImage = `src('${this.currentSrc}')`
-        mainGame.style.backgroundImage = clickedImage
+        clickedImage = this.currentSrc
+        mainGame.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) 100%),
+        url(${clickedImage})`;
+        mainGame.style.backgroundSize= "cover"
+
+        gameName.style.opacity = 0
+        gameDescription.style.opacity = 0
         
 
     })
